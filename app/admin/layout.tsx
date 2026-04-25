00e107e9ac0ae8855bb5c1edd9bc2ffa1f3ff27c
Scaleupally Developers <69164652+Devscaleupally@users.noreply.github.com>
2026-04-25 17:10:32 +0530

    admin layout created

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 
 
 
@@ -13,11 +14,74 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
-        
+        {/* <!-- bootstrap 3.0.2 --> */}
+        <link href="/admin/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        {/* <!-- font Awesome --> */}
+        <link href="/admin/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        {/* <!-- Ionicons --> */}
+        <link href="/admin/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+        {/* <!-- jvectormap --> */}
+        <link href="/admin/css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+        {/* <!-- fullCalendar --> */}
+        <link href="/admin/css/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" />
+        {/* <!-- Daterange picker --> */}
+        <link href="/admin/css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+        {/* <!-- bootstrap wysihtml5 - text editor --> */}
+        <link href="/admin/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+        {/* <!-- Theme style --> */}
+        <link href="/admin/css/AdminLTE.css" rel="stylesheet" type="text/css" />
+
       </head>
-      <body className="">{children}</body> 
+      <body className="skin-blue">
+        {children}
+
+        {/* Keep legacy admin JS isolated to admin layout only */}
+        <Script src="/admin/js/jquery.min.js" strategy="afterInteractive" />
+        <Script
+          src="/admin/js/jquery-ui-1.10.3.min.js"
+          strategy="afterInteractive"
+        />
+        <Script src="/admin/js/bootstrap.min.js" strategy="afterInteractive" />
+        <Script
+          src="/admin/js/plugins/morris/morris.min.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="/admin/js/plugins/sparkline/jquery.sparkline.min.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="/admin/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="/admin/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="/admin/js/plugins/fullcalendar/fullcalendar.min.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="/admin/js/plugins/jqueryKnob/jquery.knob.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="/admin/js/plugins/daterangepicker/daterangepicker.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="/admin/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="/admin/js/plugins/iCheck/icheck.min.js"
+          strategy="afterInteractive"
+        />
+        <Script src="/admin/js/app.js" strategy="afterInteractive" />
+      </body>
     </html>
   );
 }
